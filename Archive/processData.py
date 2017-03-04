17e39280ae408b35f92edb761599e4d2187ee30f
Michelle Lam <michelle123lam@gmail.com>
2017-03-04 15:47:39 -0800

    Splits data across multiple, smaller json files

--- Archive/processData.py
+++ Archive/processData.py
@@ -5,25 +5,40 @@ import argparse
 # Loads and processes the Enron Organizational Hierarchy json data
 # CS 224N Winter 2017
 
-def writeJson(old_filename, new_filename):
+def writeJson(old_filename, new_filename, n_per_file):
   """Write proper version of json"""
   with open(old_filename, 'r') as f:
-    with open(new_filename, 'w') as g:
-      print >> g, "{" # Opening bracket
-      i = 0
-      for x in f:
-        x = x.rstrip()
-        if not x: continue
-        # Add entry with index as key; line item as value
-        new_x = "\"%d\" : %s" % (i, x)
-        # Place comma between subsequent entries
-        if i > 0:
-          new_x = "," + new_x
-        print >> g, new_x
-        i += 1
-      print >> g , "}" # Closing bracket
+    i = 0
+    file_i = 0
+    g = None
+    for x in f:
+      if i % n_per_file == 0:
+        # Wrap up the current json file
+        if g is not None:
+          print >> g , "}" # Closing bracket
+          g.close()
+
+        # Open and start writing to new json file
+        file_i += 1
+        cur_new_filename = "%s_%d.json" % (new_filename, file_i)
+        g = open(cur_new_filename, 'w')
+        print >> g, "{" # Opening bracket
+
+      x = x.rstrip()
+      if not x: continue
+
+      # Add entry with index as key; line item as value
+      new_x = "\"%d\" : %s" % (i, x)
+
+      # Place comma between subsequent entries
+      if (i % n_per_file) > 0:
+        new_x = "," + new_x
+      print >> g, new_x
+      i += 1
+    print >> g , "}" # Closing bracket
 
   print "Successfully wrote to %s!" % (new_filename)
+
 
 def readJson(filename):
   with open(filename) as data_file:
@@ -45,9 +60,10 @@ def main():
   args = process_command_line()
 
   if args.write_json:
-    writeJson('enron_database/emails.json', 'enron_database/emails_fixed.json')
-    writeJson('enron_database/entities.json', 'enron_database/entities_fixed.json')
-    writeJson('enron_database/threads.json', 'enron_database/threads_fixed.json')
+    n_per_file = 50000 # Number of items per json file
+    writeJson('enron_database/emails.json', 'enron_database/emails_fixed', n_per_file)
+    writeJson('enron_database/entities.json', 'enron_database/entities_fixed', n_per_file)
+    writeJson('enron_database/threads.json', 'enron_database/threads_fixed', n_per_file)
   if args.read_json:
     readJson('enron_database/emails_fixed.json')
     readJson('enron_database/entities_fixed.json')
