4115726217b4896047a1c362fb838b65bd866d01
Michelle Lam <michelle123lam@gmail.com>
2017-03-04 21:44:45 -0800

    Creates bag-of-words features for emails; saves output to json

--- Archive/createFeatVecs.py
+++ Archive/createFeatVecs.py
@@ -4,6 +4,8 @@
 
 import json
 import argparse
+import string
+import re
 
 def get_power_labels():
   """Returns power labels for each email"""
@@ -13,29 +15,49 @@ def get_power_labels():
 
 def bag_of_words_features():
   """Returns bag-of-words features for each email"""
-  feats = []
+  feats = {}
+  filename = "enron_database/emails_fixed.json"
 
-  emails = {}
-  for file_i in range(1, 2):
-    # Load json; add its data to emails list
-    cur_filename = "enron_database/emails_fixed_%d.json" % (file_i)
-    f = open(cur_filename, 'r')
-    data = json.load(f)
-    emails.update(data)
+  with open(filename, 'r') as f:
+    emails = json.load(f) # dictionary of emails
 
   # TODO: ignore indices that don't have power label
 
+  punctuation = ''.join(string.punctuation)
+  whitespace_punc_regex1 = r'([a-zA-Z0-9])([' + punctuation + '])'
+  whitespace_punc_regex2 = r'([' + punctuation + '])([a-zA-Z0-9])'
+
   for email_id, email in emails.iteritems():
     words = []
+
+    # Discard emails without necessary fields
+    if "subject" not in email or "body" not in email or "recipients" not in email or len(email["recipients"]) != 1 or "sender" not in email:
+      continue
+
+    # Process email subject
     subject = email["subject"]
-    body = email["body"]
-    print "email_id", email_id
     subject = subject.replace('\n', '')
+    subject = re.sub(whitespace_punc_regex1, r'\1 \2', subject)
+    subject = re.sub(whitespace_punc_regex2, r'\1 \2', subject)
+    words.extend(subject.split())
+
+    # Process email subject
+    body = email["body"]
     body = body.replace('\n', '')
-    # TODO: more processing of the string? punctuation?
-    words.append(subject.split())
-    words.append(body.split())
-    feats.append(words) # add email's bag-of-words to feature vector
+    body = re.sub(whitespace_punc_regex1, r'\1 \2', body)
+    body = re.sub(whitespace_punc_regex2, r'\1 \2', body)
+    words.extend(body.split())
+
+    recipient = email["recipients"][0] # TODO: handle multiple recipients
+    sender = email["sender"]
+
+    # key = (int(email_id), int(sender), int(recipient))
+    key = "(" + str(email_id) + ", " + str(sender) + ", " + str(recipient) + ")" # key for json
+    feats[key] = words # add email's bag-of-words to feature vector
+
+  # Save to json
+  with open('feat_vecs.json', 'w') as fp:
+    json.dump(feats, fp)
 
   return feats
 
@@ -55,7 +77,14 @@ def main():
   if args.is_bow:
     labels = get_power_labels()
     feat_vecs = bag_of_words_features()
-    print "feat_vecs:", feat_vecs[:5]
+
+    # TEMP: print the first 5 feature vectors
+    i = 0
+    for key in feat_vecs:
+      if i > 5:
+        break
+      print key, feat_vecs[key]
+      i += 1
 
 
 if __name__ == "__main__":
