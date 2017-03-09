da1fc144b5978a7ed4c9ee0f7c9c3c7f0a2c7631
angelakong <akong2@stanford.edu>
2017-03-09 03:27:20 -0800

    CNN baseline

--- Archive/processData.py
+++ Archive/processData.py
@@ -4,6 +4,8 @@
 
 import json
 import argparse
+import numpy as np
+import re
 
 def writeSplitJson(old_filename, new_filename, n_per_file):
   """Write proper version of json"""
@@ -76,6 +78,58 @@ def process_command_line():
   args = parser.parse_args()
   return args
 
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_data_and_labels(email_contents_file, labels_file):
+    """
+    Splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    x_text = np.load(email_contents_file)
+    x_text = [clean_str(email.strip()) for email in x_text]  # Split by words and clean with regex
+    labels = np.load(labels_file)
+    # x_text contains an array of strings for all examples
+    # y contains an array of labels for all examples
+    return [x_text, labels]
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+  """
+  Generates a batch iterator for a dataset.
+  """
+  data = np.array(data)
+  data_size = len(data)
+  num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+  for epoch in range(num_epochs):
+      # Shuffle the data at each epoch
+      if shuffle:
+          shuffle_indices = np.random.permutation(np.arange(data_size))
+          shuffled_data = data[shuffle_indices]
+      else:
+          shuffled_data = data
+      for batch_num in range(num_batches_per_epoch):
+          start_index = batch_num * batch_size
+          end_index = min((batch_num + 1) * batch_size, data_size)
+          yield shuffled_data[start_index:end_index]
 
 def main():
   args = process_command_line()
