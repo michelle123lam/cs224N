cba33401a4cc397455c9c93fc9b80b7d668f8f0c
angelakong <akong2@stanford.edu>
2017-03-11 23:09:35 -0800

    Load glove vector word vecs

--- Archive/processData.py
+++ Archive/processData.py
@@ -140,6 +140,63 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
           end_index = min((batch_num + 1) * batch_size, data_size)
           yield shuffled_data[start_index:end_index]
 
+def load_embedding_vectors_word2vec(vocabulary, filename, binary):
+    # load embedding_vectors from the word2vec
+    encoding = 'utf-8'
+    with open(filename, "rb") as f:
+        header = f.readline()
+        vocab_size, vector_size = map(int, header.split())
+        # initial matrix with random uniform
+        embedding_vectors = np.random.uniform(-0.25, 0.25, (len(vocabulary), vector_size))
+        if binary:
+            binary_len = np.dtype('float32').itemsize * vector_size
+            for line_no in range(vocab_size):
+                word = []
+                while True:
+                    ch = f.read(1)
+                    if ch == b' ':
+                        break
+                    if ch == b'':
+                        raise EOFError("unexpected end of input; is count incorrect or file otherwise damaged?")
+                    if ch != b'\n':
+                        word.append(ch)
+                word = str(b''.join(word), encoding=encoding, errors='strict')
+                idx = vocabulary.get(word)
+                if idx != 0:
+                    embedding_vectors[idx] = np.fromstring(f.read(binary_len), dtype='float32')
+                else:
+                    f.seek(binary_len, 1)
+        else:
+            for line_no in range(vocab_size):
+                line = f.readline()
+                if line == b'':
+                    raise EOFError("unexpected end of input; is count incorrect or file otherwise damaged?")
+                parts = str(line.rstrip(), encoding=encoding, errors='strict').split(" ")
+                if len(parts) != vector_size + 1:
+                    raise ValueError("invalid vector on line %s (is this really the text format?)" % (line_no))
+                word, vector = parts[0], list(map('float32', parts[1:]))
+                idx = vocabulary.get(word)
+                if idx != 0:
+                    embedding_vectors[idx] = vector
+        f.close()
+        return embedding_vectors
+
+
+def load_embedding_vectors_glove(vocabulary, filename, vector_size):
+    # load embedding_vectors from the glove
+    # initial matrix with random uniform
+    embedding_vectors = np.random.uniform(-0.25, 0.25, (len(vocabulary), vector_size))
+    f = open(filename)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], dtype="float32")
+        idx = vocabulary.get(word)
+        if idx != 0:
+            embedding_vectors[idx] = vector
+    f.close()
+    return embedding_vectors
+
 def main():
   args = process_command_line()
 
